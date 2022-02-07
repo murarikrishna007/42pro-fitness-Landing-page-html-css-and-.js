7b641795e1d089c29b235ebd88bbae9509cb6e02
murarikrishna007 <murarikrishna0071994@gmail.com>
2022-02-08 01:43:40 +0530

    fitness landing page build in html and css

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello archna kumai");
+
+function toggle() {
+    const toggle1 = document.querySelector(".toggle");
+    const navigation = document.querySelector(".navigation");
+    toggle1.classList.toggle("active");
+    navigation.classList.toggle("active");
+
+}
